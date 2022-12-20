d09ec83e884f35af498ef869300ee5eb7c73cb60
Sahil Tomar <113227437+sahiltomar1210@users.noreply.github.com>
2022-12-21 00:59:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const bodyParser = require("body-parser")
 const app = express();
 const data = require("./Mock-data/data.json");
 
-app.use(bodyParser());
 
-app.get("/data",(req,res)=>{
+app.get("/",(req,res)=>{
     res.status(200).json({data})
-})
-app.get("*",(req,res)=>{
-    res.send("NO DATA")
 })
 
 app.listen(3004,()=>console.log("server is running at 3004"));
