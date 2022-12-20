2a488435eb3b5779a8820feeed60d050d495b515
Sahil Tomar <113227437+sahiltomar1210@users.noreply.github.com>
2022-12-21 00:33:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express();
+const data = require("./Mock-data/data.json");
+
+app.use(bodyParser());
+
+app.get("/data",(req,res)=>{
+    res.status(200).json({data})
+})
+
+app.listen(3004,()=>console.log("server is running at 3004"));
